4829474b5d8393bedaea5eae25188d3ae9fbe8ca
pKietDE <phanmaiquockiet2004@gmail.com>
2025-05-30 16:57:14 +0700

    initialize project FE

---
+++ src/pages/luong/luong.jsx
@@ -0,0 +1,10 @@
+export default function Luong() {
+    return (
+        <div className ='container-column'>
+            <div className='row row-1'>Lương</div>
+            <hr />
+            <div className='row row-2'>Lương</div>
+        </div>
+
+    )
+}
---
+++ src/pages/nghiphep/nghi_phep.jsx
@@ -0,0 +1,10 @@
+export default function NghiPhep() {
+    return (
+        <div className ='container-column'>
+            <div className='row row-1'>Nghỉ phép</div>
+            <hr />
+            <div className='row row-2'>Nghỉ phép</div>
+        </div>
+
+    )
+}
